a576ef60f3d6752c8bc711c4bc06c414b1174b4e
Sicnarf10 <10sicnarf@gmail.com>
2025-09-19 00:10:09 +0530

    changed the setttings.py file again v5

--- jewelry_pos/jewelry_pos/wsgi.py
+++ jewelry_pos/jewelry_pos/wsgi.py
@@ -7,10 +7,22 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
-import os
+# import os
 
+# from django.core.wsgi import get_wsgi_application
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jewelry_pos.settings')
+
+# application = get_wsgi_application()
+
+
+
+# jewelry_pos/wsgi.py
+import os
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jewelry_pos.settings')
 
 application = get_wsgi_application()
+application = WhiteNoise(application, root=os.path.join(os.path.dirname(__file__), 'staticfiles'))
