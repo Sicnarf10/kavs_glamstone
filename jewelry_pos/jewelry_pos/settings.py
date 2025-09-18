57b868e376a026afd759377b1d454cec23dd6b3b
Sicnarf10 <10sicnarf@gmail.com>
2025-09-18 19:11:06 +0530

    Configure PostgreSQL for production

--- jewelry_pos/jewelry_pos/settings.py
+++ jewelry_pos/jewelry_pos/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -85,11 +86,19 @@ WSGI_APPLICATION = 'jewelry_pos.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        # Feel free to alter this value to suit your needs.
+        default='sqlite:///db.sqlite3', # Keep SQLite for local development
+        conn_max_age=600
+    )
 }
 
 
