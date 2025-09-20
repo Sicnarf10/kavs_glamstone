ec25ed0c3e5327fc58224d42da7199875c4326f6
Sicnarf10 <10sicnarf@gmail.com>
2025-09-20 23:30:09 +0530

    just trying

--- jewelry_pos/jewelry_pos/settings.py
+++ jewelry_pos/jewelry_pos/settings.py
@@ -14,8 +14,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-cq027(*_*hihc0&-sm$6qtw5@29=rkys!q938mi=^44snzv@92')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = os.environ.get('DEBUG', 'False') == 'True'
-DEBUG = 'True'
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+# DEBUG = 'True'
 
 ALLOWED_HOSTS = [
     'kavs-glamstone.onrender.com',
@@ -82,6 +82,13 @@ DATABASES = {
     )
 }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -139,12 +146,14 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # CORS Headers Configuration
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
+    "https://localhost:5127",
     "https://kavsglamstone.netlify.app",
 ]
 
-CORS_ALLOW_CREDENTIALS = True
+# CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_REDIRECTS = True
-    
+
+   
 
 # Media files
 MEDIA_URL = '/media/'
